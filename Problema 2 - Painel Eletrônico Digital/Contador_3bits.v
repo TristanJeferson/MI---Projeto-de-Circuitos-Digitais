6d9df202fb5a4ae868021b0b265cc2bcdf941499
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2023-05-06 22:10:59 -0300

    Update Contador_3bits.v

--- Problema 2 - Painel Eletrônico Digital/Contador_3bits.v
+++ Problema 2 - Painel Eletrônico Digital/Contador_3bits.v
@@ -1,14 +1,9 @@
 module Contador_3bits(clk, q);
 	input clk;
-	output reg [2:0] q;
-	wire t0, t1, t2;
+	output t0, t1, t2;
 
-	FlipFlop_T ff0(clk, 1'b1, t0);
-	FlipFlop_T ff1(clk, t0, t1);
-	FlipFlop_T ff2(clk, t0 & t1, t2);
-	
-	always @(*) begin
-		q = {t2, t1, t0};
-	end
+	FilpFlop_T ff0(clk, 1'b1, t0);
+	FilpFlop_T ff1(clk, t0, t1);
+	FilpFlop_T ff2(clk, t0 & t1, t2);
 	
 endmodule
