155220d6d7d6d441b4c4d74c0de656ac458aab77
Fabrício Portela <44996730+fabricioportela@users.noreply.github.com>
2023-05-08 01:06:08 -0300

    Update LinhasMatriz.v

--- Problema 2 - Painel Eletrônico Digital/LinhasMatriz.v
+++ Problema 2 - Painel Eletrônico Digital/LinhasMatriz.v
@@ -1,11 +1,9 @@
-module LinhasMatriz(ch0, ch1, clkRU, clkC, outDEM0, outDEM1, outDEM2, outDEM3, outDEM4);
+module LinhasMatriz(ch0, ch1, sel1, sel2, sel3, clkRU, outDEM0, outDEM1, outDEM2, outDEM3, outDEM4);
 
-	input ch0, ch1, clkRU, clkC;
+	input ch0, ch1, clkRU, sel1, sel2, sel3;
 	wire [6:0] outRU0, outRU1, outRU2, outRU3, outRU4;
-	wire sel1, sel2, sel3;
 	output outDEM0, outDEM1, outDEM2, outDEM3, outDEM4;
 	
-	Contador(clkC, sel1, sel2, sel3);
 	bancoRU (ch0, ch1, clkRU, outRU0, outRU1, outRU2, outRU3, outRU4);
 	bancoDEMUX (sel1, sel2, sel3, outRU0, outRU1, outRU2, outRU3, outRU4, outDEM0, outDEM1, outDEM2, outDEM3, outDEM4);
 	
