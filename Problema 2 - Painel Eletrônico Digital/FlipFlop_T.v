c1bb12970354702d420b335ee92119bfdaf2dc2f
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2023-05-10 14:00:17 -0300

    Update FlipFlop_T.v

--- Problema 2 - Painel Eletrônico Digital/FlipFlop_T.v
+++ Problema 2 - Painel Eletrônico Digital/FlipFlop_T.v
@@ -1,17 +1,11 @@
 module FlipFlop_T(clk, t, q);
 	input clk, t;
 	output reg q;
-	wire not_T, not_Q, and1, and2, w1;
-	
-	not (not_T, t);
-	not (not_Q, q);
-	
-	and (and1, not_T, q);
-	and (and2, not_Q, t);
-	
-	or(w1, and1, and2);
 	
 	always @(posedge clk) begin
-		q <= w1;
+		if (t)
+			q <= ~q;
+		else
+			q <= q;
 	end
 endmodule
