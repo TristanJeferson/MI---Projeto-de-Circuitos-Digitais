942f0e2992ff7cc6213153f100024bc2bafb595d
vitoriatanan <128485508+vitoriatanan@users.noreply.github.com>
2023-05-10 17:31:49 -0300

    Update Contador.v

--- Problema 2 - Painel Eletrônico Digital/Contador.v
+++ Problema 2 - Painel Eletrônico Digital/Contador.v
@@ -1,9 +1,12 @@
 module Contador(clk, t2, t1, t0);
 	input clk;
 	output t0, t1, t2;
-
-	FlipFlop_T ff0(clk, 1'b1, t0);
-	FlipFlop_T ff1(clk, t0, t1);
-	FlipFlop_T ff2(clk, t0 & t1, t2);
+	wire rst;
+	
+	and (rst, t0, t1, t2);
+	
+	FlipFlop_T(clk, rst, 1, t0);
+	FlipFlop_T(t0, rst, 1, t1);
+	FlipFlop_T(clk, rst, 1, t2);
 	
 endmodule
